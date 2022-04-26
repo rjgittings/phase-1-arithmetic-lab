f58a6bbcb5e269fbc5d25723b1e9e7842928055a
Robert Gittings <rjgitting@gmail.com>
2022-04-25 20:22:29 -0400

    commit tree c4f69190d2a14ab02f370cbd2d1010d74225a722

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2 
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 4
+const num4 = 16
+const mod = num4 / num3
+
+const max = Math.max(1, 2, 3, 4, 5, 6, 13, 20);
+
